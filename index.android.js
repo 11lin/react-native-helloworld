a353d20908a3340650dff6b10e4b0944d058bc0b
li <jcfss@126.com>
2016-11-05 21:47:29 +0800

    闪烁的效果

--- index.android.js
+++ index.android.js
@@ -20,6 +20,26 @@ class Greeting extends Component {
     );
   }
 }
+//闪烁的效果
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+  }, 1000);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
 export default class AwesomeProject extends Component {
   render() {
       let pic = {
@@ -33,6 +53,11 @@ export default class AwesomeProject extends Component {
               <Greeting name='张三' />
               <Greeting name='李四' />
               <Greeting name='王二' />
+
+              <Blink text='I love to blink' />
+              <Blink text='Yes blinking is so great' />
+              <Blink text='Why did they ever take this out of HTML' />
+              <Blink text='Look at me look at me look at me' />
           </View>
       );
   }
