dab714858a0ff87c13c78d3fcc80131a3b983b5d
li <jcfss@126.com>
2016-11-05 21:34:33 +0800

    自定义组件

--- index.android.js
+++ index.android.js
@@ -12,13 +12,28 @@ import {
   View,
   Image
 } from 'react-native';
+//自定义组件
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 export default class AwesomeProject extends Component {
   render() {
       let pic = {
         uri: 'http://uhero.cc/uploads/images/qjbj.jpg'
       };
       return (
-        <Image source={pic} style={{width: 193, height: 110}} />
+          <View>
+              <Text>Hello World!</Text>
+              <Image source={pic} style={{width: 200, height: 200}} />
+
+              <Greeting name='张三' />
+              <Greeting name='李四' />
+              <Greeting name='王二' />
+          </View>
       );
   }
 }
